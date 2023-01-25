410f694d8f8ac16db5e1d80a46b31f6d80bf2719
rubencc <rcrespoceballos@outlook.com>
2023-01-25 11:39:40 +0100

    csv change

--- csv_merge.py
+++ csv_merge.py
@@ -3,17 +3,19 @@ import glob
 import pandas as pd
 
 #changes the current directory to the specified path
-os.chdir(r"C:\Users\ruben.crespo\Documents\03_tests\MODIS_burned_area\modis\2020")
+os.chdir(r"C:\Users\admin\Documents\01_Ruben_Scripts\im.nca.postprocessing\aggregation.region\vegetation.carbon.stock\tmp\vcs.aggregated.country")
 
 extension = 'csv'
 #We create a list. {}= takes the name of the file.csv in format extension
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
-#combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+#combine all files in the list up to down
+# combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
 
+#combine on the same axis
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ], axis=1)
 
 #export to csv in working folder
-combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+combined_csv.to_csv( "combined.csv", index=False, encoding='utf-8-sig')
 
 print("congratulations, the script is over")
