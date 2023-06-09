2e482fb754a2ea15d041e8e57ebbc7500a649412
Ruben <rcrespoceballos@outlook.com>
2023-06-09 21:34:25 +0200

    Auto stash before merge of "main" and "bitbucket_origin/main"

--- csv_merge.py
+++ csv_merge.py
@@ -3,13 +3,14 @@ import glob
 import pandas as pd
 
 #changes the current directory to the specified path
-os.chdir(r"C:\Users\ruben.crespo\Documents\02_Ruben_scripts\Python_codigo\im.nca.postprocessing\aggregation.region\vegetation.carbon.stock\tmp\vcs.aggregated.country")
+# os.chdir(r"C:\Users\ruben.crespo\Documents\02_Ruben_scripts\Python_codigo\im.nca.postprocessing\aggregation.region\vegetation.carbon.stock\tmp\vcs.aggregated.country")
+os.chdir(r"C:\Users\ruben.crespo\Downloads\data")
 
 extension = 'csv'
 #We create a list. {}= takes the name of the file.csv in format extension
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
-#combine all files in the list up to down
+#combine all files on the same column / in the list up to down
 # combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
 
 #combine on the same axis
